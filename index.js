276e8e55153d0ead442e3028dd079b3348714798
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-07-26 18:43:32 -0700

    added packages and questions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
-
+const inquierer = require('inquirer');
+const colors = require('colors');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your username?', "What is your password?", 'Re-enter password to confirm', ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
