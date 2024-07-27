d3e5af9ef0d2cb884049d58826d32a13e26f2fda
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-07-26 20:36:44 -0700

    made license return none

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is your username?",
+        message: "What is your  GitHub username?",
     },
 
     {
